db321172286539df9a27b0219a4c6c8c9055520f
Job85 <julianjernigan@ymail.com>
2022-09-09 22:59:17 -0400

    setup middleware with mongoose to connect to mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+
 const app = express();
 
 const PORT = process.env.PORT;
+const PASSWORD = process.env.DB_PASSWORD;
+const USERNAME = process.env.DB_USERNAME;
+
+const dbUrl = `mongodb+srv://${USERNAME}:${PASSWORD}@chatcluster.dsf3xui.mongodb.net/?retryWrites=true&w=majority`
 
 app.use(express.static(__dirname));
 
 const server = app.listen(`${PORT}`, () => {
     console.log(`Server is running on PORT, ${PORT}!`);
+})
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongoDB connected', err);
 })
