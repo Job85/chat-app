67e77a9917d35d489ec77149eda156499e6d2ad5
Job85 <julianjernigan@ymail.com>
2022-09-09 22:38:36 -0400

    installed nodemon dependency

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(express.static(__dirname));
+
 const server = app.listen(`${PORT}`, () => {
     console.log(`Server is running on PORT, ${PORT}!`);
 })
